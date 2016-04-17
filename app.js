a54c939434a4511149ee7a7b04fbf7c7e3ae4de3
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-04-17 12:40:20 +0900

    Apply access_token and call sendTextMessage function

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ app.post('/', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      //sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
   }
   res.sendStatus(200);
 });
 
-var token = "";
+var token = "CAADFDsM264QBAAUw6ZBbRhFpn7nruTvzl5vauWl3xYlZCQ5E4bNQvsi2yZA8IpqKCSFV4APOUaeZCZAKCLYCRMsrcbsO7NTy0DWcT0n2WTfZAjNcCD4IdZA2uvgh0e1bahW5VZCjZCO2Q7m934S6STety18AzSRZCdxGMkKmcmjfxWimG5Bc3b2ZBZC1r3LEOxg9aoqCNWuoRLQrdQZDZD";
 
 function sendTextMessage(sender, text) {
   messageData = {
