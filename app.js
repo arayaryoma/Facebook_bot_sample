92397099c657c865494504547b86c0a66c999ef9
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-04-17 13:09:05 +0900

    Show req as log

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
+    console.log(req)
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
