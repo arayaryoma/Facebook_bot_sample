184730f2486e8faa121453b7b5a3d87b325a75aa
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-04-17 13:05:54 +0900

    Output log

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.post('/', function (req, res) {
 var token = "CAADFDsM264QBAAUw6ZBbRhFpn7nruTvzl5vauWl3xYlZCQ5E4bNQvsi2yZA8IpqKCSFV4APOUaeZCZAKCLYCRMsrcbsO7NTy0DWcT0n2WTfZAjNcCD4IdZA2uvgh0e1bahW5VZCjZCO2Q7m934S6STety18AzSRZCdxGMkKmcmjfxWimG5Bc3b2ZBZC1r3LEOxg9aoqCNWuoRLQrdQZDZD";
 
 function sendTextMessage(sender, text) {
+    console.log(sender);
+    console.log(text);
   messageData = {
     text:text
   }
